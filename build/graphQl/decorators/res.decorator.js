4b0bb4b08f5baa2e3b8d11d1e0e46991ab18403c
danixl30 <75582933+danixl30@users.noreply.github.com>
2022-10-29 09:59:10 -0400

    Delete build directory

--- build/graphQl/decorators/res.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Res = void 0;
-const type_graphql_1 = require("type-graphql");
-function Res() {
-    return (0, type_graphql_1.createParamDecorator)(({ context }) => context.res);
-}
-exports.Res = Res;
